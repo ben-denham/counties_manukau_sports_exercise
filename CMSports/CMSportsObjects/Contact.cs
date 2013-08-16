368e86527342a3b72cad03c558d5083f72307d09
Dieuwe de Boer <dieuwe@pobox.com>
2013-08-16 20:28:42 +1200

    Attempt at validation

--- CMSports/CMSportsObjects/Contact.cs
+++ CMSports/CMSportsObjects/Contact.cs
@@ -46,7 +46,7 @@ namespace CMSportsObjects
             }
             set
             {
-                string regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$";
+                string regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$";
                 if (!Regex.IsMatch(value, @regex, RegexOptions.IgnoreCase)) {
                     throw new NotImplementedException("The email address you entered is not valid.");
                 }
